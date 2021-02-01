fc3095e99a2c87e0e6fbc7c6860c89dcce00a0b6
Olexandr Draiuk <alexdrajuk@gmail.com>
2021-02-01 23:14:54 +0200

    Update svg-sprite

--- scripts/svg-sprite.js
+++ scripts/svg-sprite.js
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./svg-sprite.css">
-    <title>SVG Sprite</title>
-</head>
-<body>
+(function() {
+    const sprite = `
     <svg style="display: none">
         <defs>
             <symbol id="svg-sprite__close" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
@@ -27,7 +20,19 @@
                 <path d="M12.258 8.024a2 2 0 00-2.225 1.308 1 1 0 11-1.886-.664 4 4 0 017.773 1.333c0 1.53-1.135 2.54-1.945 3.081a8.041 8.041 0 01-1.686.848l-.035.013-.011.003-.004.002h-.002L11.92 13l.316.949a1 1 0 01-.633-1.897H11.6h.002l.016-.006.074-.027a6.051 6.051 0 001.172-.6c.69-.46 1.055-.95 1.055-1.419v-.001a2 2 0 00-1.662-1.975zM12 15a1 1 0 011 1v.5a1 1 0 11-2 0V16a1 1 0 011-1z"/>
                 </g>
             </symbol>
+            <symbol id="svg-sprite__menu" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M2 12a1 1 0 011-1h18a1 1 0 110 2H3a1 1 0 01-1-1zM2 6a1 1 0 011-1h18a1 1 0 110 2H3a1 1 0 01-1-1zM2 18a1 1 0 011-1h18a1 1 0 110 2H3a1 1 0 01-1-1z" />
+            </symbol>
+            <symbol id="svg-sprite__check" viewBox="0 0 15 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M1.5 7.5L5 11L13 2" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+            </symbol>
         </defs>
     </svg>
-</body>
-</html>
+    `;
+    
+    const template = document.createElement('template');
+    template.innerHTML = sprite.trim();
+    const body = document.querySelector('body');
+    body.insertBefore(template.content.firstChild, body.firstElementChild);
+})();
+
